ceb5aa7754dfb37cabe9e244a1248a23ae0ac05d
Livia Seibert <livia.seibert@duke.edu>
2020-07-18 21:11:13 -0400

    more

--- screens/WelcomeScreen.tsx
+++ screens/WelcomeScreen.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
 import { StyleSheet, TextInput, TouchableOpacity } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
 export default function Welcome() {
