15d012a848ec1a407eaf7c17deef8d22f3dbe1ba
Rachel Luria <rachel.luria@duke.edu>
2020-07-18 21:20:50 -0700

    health

--- screens/OnboardingHealth.tsx
+++ screens/OnboardingHealth.tsx
@@ -5,6 +5,15 @@ import { AppLoading } from 'expo';
 import { useFonts, NunitoSans_300Light, NunitoSans_400Regular, NunitoSans_900Black } from '@expo-google-fonts/nunito-sans';
 import { useNavigation } from '@react-navigation/native';
 import { transform } from '@babel/core';
+import RNPickerSelect from 'react-native-picker-select';
+import { RadioButton } from 'react-native-paper';
+
+
+function onSelectedItemsChange(){
+    return;
+  };
+
+var items = [{name: "Type I Diabetes"}, {name: "Type II Diabetes"}]
 
 export default function OnboardingHealth() {
   const navigation = useNavigation();
@@ -14,6 +23,8 @@ export default function OnboardingHealth() {
     NunitoSans_900Black,
   });
 
+  const [checked, setChecked] = React.useState('first')
+
   if (!fontsLoaded) {
     return <AppLoading />;
   } else {
@@ -21,8 +32,38 @@ export default function OnboardingHealth() {
       <View>
         <Text style={styles.headertext}>DIAGNOSIS</Text>
         <Text style={styles.successtext}>Please select your medical diagnosis.</Text>
+        <View style={{top: 110, width:250, left:70}}>
+        <RNPickerSelect
+            onValueChange={(value) => console.log(value)}
+            items={[
+                { label: 'Type I Diabetes', value: 'Type I Diabetes' },
+                { label: 'Type II Diabetes', value: 'Type II Diabetes' },
+            ]}
+        />
+        </View>
+
         <Text style={styles.headertext2}>MEDICATION</Text>
         <Text style={styles.successtext2}>Are you currently taking any perscription medication?</Text>
+        <View style={{top: 240, width:250, left:70, opacity:0.5}}>
+        <RadioButton
+        value="Yes"
+        status={ checked === 'Yes' ? 'checked' : 'unchecked' }
+        onPress={() => setChecked('first')}
+        uncheckedColor="#4F4F4F"
+        color="#4F4F4F"
+        />
+        <RadioButton
+        value="No"
+        status={ checked === 'No' ? 'checked' : 'unchecked' }
+        onPress={() => setChecked('second')}
+        uncheckedColor='#4F4F4F'
+        color="#4F4F4F"
+        />
+
+        <Text style={styles.successtext3}>Please input your medication.</Text>
+        <TextInput style={{left:70, top: 90}}>Medication Name</TextInput>
+
+        </View>
         <TouchableOpacity style={styles.arrowright} onPress={() =>
           navigation.navigate('OnboardingFeedback')}>
         </TouchableOpacity>
@@ -116,5 +157,20 @@ const styles = StyleSheet.create({
     borderStyle: 'solid',
     borderColor: '#828282',
     transform: [{ rotate: '45deg' }],
+  },
+  successtext3: {
+    position: 'absolute',
+    width: 243,
+    height: 87.75,
+    left: 50,
+    top: 120,
+    fontFamily: 'NunitoSans_300Light',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    fontSize: 13,
+    lineHeight: 22.5,
+    textAlign: 'left',
+    color: '#4F4F4F',
+    marginTop: 10,
   },
 });
